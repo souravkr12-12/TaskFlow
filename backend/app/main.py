15b22c0a26433cef48d78f288e3f49bcdf01cb42
Sourav Kumar <souravkumarmgr72@gmail.com>
2026-07-04 17:46:47 +0530

    Create task database model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-
+from app.database import Base,engine
+from app.models import Task
 app=FastAPI(
   title="TaskFlow API",
   version="1.0.0"
 )
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
